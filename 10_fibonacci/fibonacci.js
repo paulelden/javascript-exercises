7ae06e9817d5d4bc5e12326b7f12878c0e7f9ed7
Paul Elden <paulelden11@gmail.com>
2022-02-13 17:38:26 -0500

    Added solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(val) {
+    let n1 = 0, n2 = 1, next;
+    if (val < 0) { return "OOPS"; }
+    for (let i = 1; i <= val; i++) {
+        next = n1 + n2;
+        n1 = n2;
+        n2 = next;
+    }
+    return n1;
 };
 
 // Do not edit below this line
