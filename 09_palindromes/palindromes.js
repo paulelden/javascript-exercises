37045303f4973a7b129081ad395e08eb752d989a
Paul Elden <paulelden11@gmail.com>
2022-02-13 16:38:43 -0500

    Created solution for excercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let alpha = Array.from('abcdefghijklmnopqrstuvwxyz');
+    let arr = str.toLowerCase().split('');
+    let arrFiltered = arr.filter(letter => alpha.includes(letter.toLowerCase()));
+    return (arrFiltered.join('') === arrFiltered.reverse().join(''));
 };
 
 // Do not edit below this line
