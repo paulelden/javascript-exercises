da209fb1462dbe639791da512b05aee2096e6fbc
Paul Elden <paulelden11@gmail.com>
2022-02-13 18:33:09 -0500

    Added solution to exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let titles = arr.map(book => {
+        console.log(book.title);
+        return book.title;
+    });
 
+    return titles;
 };
 
 // Do not edit below this line
