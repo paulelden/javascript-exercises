10578aed190b852c23c6cc7eedc2167b8efbd3b6
Paul Elden <paulelden11@gmail.com>
2022-02-13 15:31:31 -0500

    Added solution to exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if ( !arr[0] ) { 
+    return 0; 
+  }
+  else if ( !arr[1]) { 
+    return arr[0] 
+  }
+  else {
+    let total = arr.reduce(function (num1, num2) {
+      return num1 + num2;
+  });
+  return total;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((num1, num2) => { return num1 * num2} );
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let rval = 1;
+  for (let i = 2; i <= num; i++) {
+    rval = rval * i;
+  }
+  return rval;
 };
 
 // Do not edit below this line
